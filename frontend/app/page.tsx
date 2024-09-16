ade0685fe4c290428085e18a803aaca16c880679
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2024-09-17 01:58:34 +0900

    change frontend/app.tsx MyToken to ERC20.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,24 @@
 "use client"
 import { ethers } from "ethers";
-import { useEffect, useState } from "react";
-import artifact from "../abi/MyToken.sol/MyToken.json";
+import { useEffect, useState } from 'react';
+import artifact from "../abi/MyERC20.sol/MyERC20.json";
 
-// デプロイしたMyTokenのアドレス
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// デプロイしたMyERC20のアドレス
+const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 export default function Home() {
-  // Metamaskなどが提供するイーサリアムプロバイダーを格納する変数
+  // Metamask等が提供するEthereumプロバイダーを格納する変数
   const [windowEthereum, setWindowEthereum] = useState();
   // MyTokenの所有数を格納する変数
   const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
-    // イーサリアムプロバイダーを取得し、変数に代入
+    // イーサリウムプロバイダーを取得し、変数に代入
     const { ethereum } = window as any;
     setWindowEthereum(ethereum);
   }, []);
 
-  // ボタンを押下したときに実行される関数
+  // ボタンを押下した時に実行される関数
   const handleButtonClick = async () => {
     if (windowEthereum) {
       // Ethereumプロバイダーを設定
@@ -32,8 +32,8 @@ export default function Home() {
         provider
       );
       // ウォレットアドレスの取得
-      const walletAddress: string = await signer.getAddress();
-      // MyTokenコントラクトから指定したウォレットアドレスのトークン所有数を取得
+      const walletAddress : string = await signer.getAddress();
+      // MyTokenコントラクトからウォレットアドレスのトークン所有数を取得
       const balance = await contract.balanceOf(walletAddress);
       // BigIntリテラル付きで所有数が返されるのでテキストに変換して代入
       setInputValue(balance.toString());
